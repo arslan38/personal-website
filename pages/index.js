814b393166bffb21f8feeceaf07810a35118afe1
Burak Arslan <49092119+arslan38@users.noreply.github.com>
2024-07-29 18:18:59 +0100

    feat: create personal introduction page with Chakra UI components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+import { Box, Container, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="lightgray" p={3} mb={6} align="center">
+                Hello, I&apos;m a full-stack developer based in Istanbul, Turkey.
+            </Box>
+
+            <Box display={{md:"flex"}}> 
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Huzeyfe Burak Arslan
+                    </Heading>
+                    <p>Digital Craftzman ( Developer / Designer )</p>
+                </Box>
+            </Box>
+        </Container>
+        
     )
 } 
 
