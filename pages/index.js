e03c4cdbe29b221bfc78ddf14af6db5737796117
Burak Arslan <49092119+arslan38@users.noreply.github.com>
2024-07-29 20:16:34 +0100

    feat: Profile picture added to the main window

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Container, Heading } from "@chakra-ui/react"
+import { Box, Container, Heading, Image } from "@chakra-ui/react"
 
 const Page = () => {
     return (
@@ -13,6 +13,31 @@ const Page = () => {
                         Huzeyfe Burak Arslan
                     </Heading>
                     <p>Digital Craftzman ( Developer / Designer )</p>
+                </Box>
+
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                    <Box
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        w="100px"
+                        h="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        overflow="hidden"
+                    >
+                        <Image
+                            src="/images/burak.jpeg"
+                            alt="Profile image"
+                            width="100px"
+                            height="100px"
+                        />
+                    </Box>
                 </Box>
             </Box>
         </Container>
